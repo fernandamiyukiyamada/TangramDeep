c112ba02b5bd8149190b88eb8787ec388e6f8498
fernandamiyukiyamada <147230696+fernandamiyukiyamada@users.noreply.github.com>
2023-12-23 01:34:33 +0900

    Update loss_wmae.py

--- loss_wmae.py
+++ loss_wmae.py
@@ -1,11 +1,20 @@
 def wmae_loss():
     def loss(y_true, y_pred):
-        coeff = 5
-        y_true1, y_pred1 = (255 - y_true) / 255, (255 - y_pred) / 255
-        dif = y_true1 - y_pred1
-        temp1 = K.l2_normalize(K.cast(K.greater(dif, 0),"float32")*y_true1,axis=-1) * coeff
-        temp2 = K.cast(K.less(dif, 0),"float32")
-        weight = temp1 + temp2  
-        loss = K.abs(weight*dif)
-        return K.mean(loss) 
+        ## Coefficient
+        c = 5
+        
+        ## Normalization
+        y_true_norm = (255 - y_true) / 255
+        y_pred_norm = (255 - y_pred) / 255
+
+        ## Delta
+        delta = y_true_norm - y_pred_norm
+
+        ## Weighted Absolute Error
+        greater = K.cast(K.greater(delta, 0),"float32")
+        less = K.cast(K.less(delta, 0),"float32")
+        w = K.l2_normalize(greater*y_true_norm,axis=-1) * c + less
+        loss = K.mean(K.abs(w*delta))
+        
+        return loss 
     return loss
